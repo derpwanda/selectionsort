4a8b4cfc22bcf80b2349ce0c8aa528175b295c76
Wanda Evans <doubleuevans@gmail.com>
2020-07-02 20:51:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+
+function selectionsort(arr){
+    for (let i = 0; i < arr.length; i++){
+        let min = i
+        for (let j = i + 1; j < arr.length; j++){
+            if (arr[min] > arr[j]){
+                min = j
+            }
+        }
+        // SWAP
+        // if (min !== i){
+            let temp = arr[i]
+            arr[i] = arr[min]
+            arr[min] = temp
+        // }
+    }
+    return arr
+}
+
+let array = [ 2, 8, 6, 3, 4]
+console.log(selectionsort(array))
